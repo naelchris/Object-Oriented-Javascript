6af0763aa497c41d9940943d9e2614ebcecb8650
naelchris <naelchris@gmail.com>
2021-06-17 23:14:26 +0700

    refresh callback

---
+++ async/callback.js
@@ -0,0 +1,14 @@
+//callback needs to have err parameter
+//condition if there is an error
+//we have Task.<Obejct Method>
+
+//we use then with promises
+//ex : User.findById(request.userId).then(function(user){...})
+
+// async/Await example
+// async function ...(req,res){try{...}catch(error){...}}
+
+
+function endPointHandler(req, res){
+
+}
